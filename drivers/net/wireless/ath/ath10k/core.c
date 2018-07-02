9ba50fc2b5f16c6f34c66a1e6b449bc0d9e9cba2
Erik Stromdahl <erik.stromdahl@gmail.com>
2018-07-02 19:00:42 +0200

    ath10k: add htt_tx num_pending window
    Instead of turning off TX when the num_pending_tx counter has reached
    its maximum value and immediately turn it on back again when the
    counter has been decremented, we introduce a window for the number of
    pending TX frames.
    
    During high network load (like iperf tests), the TX lock will be turned
    on and off for each transmitted frame, thus eliminating the purpose of
    this counter (since there will only be one outstanding frame).
    
    The lock is also activated HTC_HOST_MAX_MSG_PER_TX_BUNDLE frames below
    the maximum value in order to make sure we don't reach the max TX pending
    limit while processing packet bundles.
    
    Signed-off-by: Erik Stromdahl <erik.stromdahl@gmail.com>

--- drivers/net/wireless/ath/ath10k/core.c
+++ drivers/net/wireless/ath/ath10k/core.c
@@ -2206,6 +2206,10 @@ static int ath10k_core_init_firmware_features(struct ath10k *ar)
 		return -EINVAL;
 	}
 
+	ar->htt.num_pending_tx_lock =
+		ar->htt.max_num_pending_tx - HTC_HOST_MAX_MSG_PER_TX_BUNDLE - 1;
+	ar->htt.num_pending_tx_unlock = ar->htt.num_pending_tx_lock / 2;
+
 	if (ar->hw_params.num_peers)
 		ar->max_num_peers = ar->hw_params.num_peers;
 	else
