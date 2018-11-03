164dc5966b34ff7c2b279563786f04faded214b0
Erik Stromdahl <erik.stromdahl@gmail.com>
2018-11-03 16:39:16 +0100

    ath10k: add more_data argument in TX chain
    This makes it possible for the SDIO and USB Hif layers to
    implement packet bundling.
    
    Signed-off-by: Erik Stromdahl <erik.stromdahl@gmail.com>

--- drivers/net/wireless/ath/ath10k/htt_tx.c
+++ drivers/net/wireless/ath/ath10k/htt_tx.c
@@ -1196,7 +1196,7 @@ err:
 	sizeof(struct ath10k_htc_hdr))
 
 static int ath10k_htt_tx_hl(struct ath10k_htt *htt, enum ath10k_hw_txrx_mode txmode,
-			    struct sk_buff *msdu)
+			    struct sk_buff *msdu, bool more_data)
 {
 	struct ath10k *ar = htt->ar;
 	int res, data_len;
@@ -1282,7 +1282,7 @@ static int ath10k_htt_tx_hl(struct ath10k_htt *htt, enum ath10k_hw_txrx_mode txm
 	 */
 	tx_desc->peerid = __cpu_to_le32(HTT_INVALID_PEERID);
 
-	res = ath10k_htc_send(&htt->ar->htc, htt->eid, msdu);
+	res = ath10k_htc_send_bundle(&htt->ar->htc, htt->eid, msdu, more_data);
 
 out:
 	return res;
@@ -1290,7 +1290,8 @@ out:
 
 static int ath10k_htt_tx_32(struct ath10k_htt *htt,
 			    enum ath10k_hw_txrx_mode txmode,
-			    struct sk_buff *msdu)
+			    struct sk_buff *msdu,
+			    bool more_data)
 {
 	struct ath10k *ar = htt->ar;
 	struct device *dev = ar->dev;
@@ -1490,7 +1491,8 @@ err:
 
 static int ath10k_htt_tx_64(struct ath10k_htt *htt,
 			    enum ath10k_hw_txrx_mode txmode,
-			    struct sk_buff *msdu)
+			    struct sk_buff *msdu,
+			    bool more_data)
 {
 	struct ath10k *ar = htt->ar;
 	struct device *dev = ar->dev;
