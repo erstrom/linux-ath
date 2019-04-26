7bbc91e59eba6c6371011713da3b7e0eb442a5f4
Erik Stromdahl <erik.stromdahl@gmail.com>
2019-04-26 07:18:56 +0200

    ath10k: add start_once support
    Add possibility to configure the driver to only start target once.
    This can reduce startup time of SDIO devices significantly since
    loading the firmware can take a substantial amount of time.
    
    The patch is also necessary for high latency devices in general
    since it does not seem to be possible to rerun the BMI phase
    (fw upload) without power-cycling the device.
    
    Signed-off-by: Erik Stromdahl <erik.stromdahl@gmail.com>

--- drivers/net/wireless/ath/ath10k/hw.h
+++ drivers/net/wireless/ath/ath10k/hw.h
@@ -613,6 +613,12 @@ struct ath10k_hw_params {
 	 * firmware bug
 	 */
 	bool uart_pin_workaround;
+
+	/* Specifies whether or not the device should be started once.
+	 * If set, the device will be started once by the early fw probe
+	 * and it will not be terminated afterwards.
+	 */
+	bool start_once;
 };
 
 struct htt_rx_desc;
